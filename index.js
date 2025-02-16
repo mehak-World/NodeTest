4affea3e175cd1e16137bfbb8273089eddf9fc1d
Mehak Narang <139652649+mehak-World@users.noreply.github.com>
2025-02-16 13:36:35 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test');
-
-const Cat = mongoose.model('Cat', { name: String });
-
-const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
-
 app.get("/", (req, res) => {
     res.send("Hi there!");
 });
