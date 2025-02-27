659e8fcf61cd5ef19a317edd55a3885277716570
Mehak Narang <139652649+mehak-World@users.noreply.github.com>
2025-02-26 21:10:13 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 
 app.get("/", (req, res) => {
-    res.send("Hi there!");
+    res.send("Web App successfully deployed on Microsoft Azure!");
 });
 
 app.listen(PORT, () => {
